a027aacca31c82be3bd3e3d4177bd6ac1ef73004
Thiago <thiagodessa41@gmail.com>
2022-05-11 11:43:19 -0300

    ManipulacaoPropriedades

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Comentario from './components/cometario';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Meu projeto</h1>
+class App extends Component {
 
-     <Comentario nome="João" email="joao@mail.com" data={new Date(2020, 3, 19)}>
-      Olá tudo bem?
-     </Comentario>
+  state={
+    comentarios:[
+      {
+      nome:'João',
+      email:'joao@mail.com',
+      data: new Date(2020, 3, 19),
+      mensagem: 'Olá, tudo bem?'
+      },
+      {
+        nome:'Pedro',
+        email:'Pedro@mail.com',
+        data: new Date(2020, 3, 20),
+        mensagem: 'Olá, tudo bem sim...'
+        }
+    ]
+  }
+ // para adicionar um novo comentario
+  adicionarComentario = () =>{
+    const novoComentario = {
+      nome: 'Maria',
+      email:'maria@gmail.com',
+      data: new Date(),
+      mensagem:'Ola pessoal..'
+    }
+    this.setState({comentarios: [...this.state.comentarios, novoComentario]})
+  }
 
-     <Comentario nome="Maria" email="maria@mail.com" data={new Date(2020, 3, 20)}>
-       Oi tudo bem sim!
-     </Comentario>        
-    </div>
-  );
+ 
+
+  render(){
+    return (
+      <div className="App">
+       <h1>Meu projeto</h1>
+      {this.state.comentarios.map((comentario,indice) =>(
+        <Comentario
+        key={indice}
+        nome={comentario.nome}
+        email={comentario.email}
+        data={comentario.data}>
+        {comentario.mensagem}
+        </Comentario>
+      ))}   
+      
+             <button onClick={this.adicionarComentario}>Adicionar um comentário</button>
+      </div>
+      );
+  }
+ 
 }
 
 export default App;
